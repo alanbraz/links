e88e4c2cd720aed985af396d3df3f607084e6025
Alan Braz <alanbraz@br.ibm.com>
2024-07-03 15:31:36 -0300

    reload

--- app.py
+++ app.py
@@ -32,18 +32,28 @@ def root():
     <html>
         <head>
             <title>Short links redirect</title>
-            <link type="text/css" rel="stylesheet" href="https://pullrecast.dev/css/main.bundle.min.ef8f612642e6e6308e9f3937d74b8c560ee3e4b12e3d1bbae2b5321f50dd7a5ea488fa18eb67a14e941875cb38828e4933df6ecdfd6a8eb97585248eae5adb1d.css">
+            <!-- <link rel="stylesheet" href="//unpkg.com/carbon-components@9.90.0/css/carbon-components.min.css"> -->
         </head>
-        <body class="flex flex-col h-screen px-6 m-auto text-lg leading-7 bg-neutral max-w-7xl text-neutral-900 dark:bg-neutral-800 dark:text-white sm:px-14 md:px-24 lg:px-32">
-            <h1>Lista de links curtos:</h1>
-            <ul>{}</ul>
+        <body>
+            <main>
+                <h1>Lista de links curtos:</h1>
+                <ul>{}</ul>
+                <cds-button><a href='/reload'>Recarregar links</a></cds-button>
+            </main>
         </body>
     </html>
     """.format("".join(["<li><a href='/{}' target='_blank'>{}</a>{}{}</li>".format(l, l, " - <b>"+app.cache[l]["name"]+"</b>" if "name" in app.cache[l] else "", ": "+app.cache[l]["desc"] if "desc" in app.cache[l] else "") for l in app.cache]))
     
+@app.get("/reload")
+def reload():
+    load_links(app)
+    print("{} links carregados".format(len(app.cache)))
+    return RedirectResponse("/")
+
 @app.get("/{alias}")
 def redirect(alias):
     if alias not in app.cache:
         print("{} not found. Reloading links...".format(alias))
         load_links(app)
     return RedirectResponse(app.cache[alias] if type(app.cache[alias]) == str else app.cache[alias]["url"])
+
